a98b7f2236ae74c8f439f10df5fc4831181b46f9
Pat Allan <pat@freelancing-gods.com>
2011-08-27 10:49:54 +1000

    Form field helper.

--- app/helpers/dobro_helper.rb
+++ app/helpers/dobro_helper.rb
@@ -5,6 +5,24 @@ module DobroHelper
     }
   end
 
+  def dobro_field_for(form, column)
+    column_name = column.name.to_sym
+    case column.type.to_sym
+    when :integer
+      form.text_field column_name, :class => 'integer'
+    when :date
+      form.text_field column_name, :placeholder => 'yyyy-mm-dd'
+    when :text
+      form.text_area column_name
+    when :boolean
+      form.check_box column_name
+    else
+      form.text_field column_name
+    end
+  end
+
+  private
+
   def hidden_dobro_column_names
     %w( id created_at updated_at )
   end
