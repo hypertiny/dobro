b9253d3d8f4de33f62ac84c115c7db53e94d15c3
Pat Allan <pat@freelancing-gods.com>
2011-08-30 12:25:33 +1000

    Updating to latest Combustion, which takes care of loading Rails routes and Capybara helpers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,6 @@ require 'rspec/rails'
 require 'capybara/rspec'
 
 RSpec.configure do |config|
-  config.include Capybara
-  config.include Rails.application.routes.url_helpers
-  config.include Rails.application.routes.mounted_helpers
-
   config.use_transactional_fixtures = true
 
   # enable filtering for examples
